e16559dce8a27ba339374a102c565da6fd4b2fac
zelr0x <zelr0xdev@gmail.com>
2019-03-02 20:09:02 +0400

    Implemented command line arguments processing allowing to pass port on start.

---
+++ cmd/gophgen/main.go
@@ -0,0 +1,31 @@
+// Package main parses command-line arguments and starts the server.
+package main
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/zelr0x/gophgen/internal/app/server"
+)
+
+const (
+	// defaultPort defines a port to use when none is specified on start.
+	defaultPort uint16 = 8080
+	// lastReserved is the highest reserved port number.
+	lastReserved = 1024
+)
+
+// main starts the server on port.
+func main() {
+	server.Serve(getPort())
+}
+
+// getPort parses command-line arguments and returns port number to use
+func getPort() uint16 {
+	if len(os.Args) > 1 {
+		if p, err := strconv.ParseUint(os.Args[1], 10, 16); err == nil && p > lastReserved {
+			return uint16(p)
+		}
+	}
+	return defaultPort
+}

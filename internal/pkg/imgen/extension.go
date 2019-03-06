a05ae60dddf2c35f78bc31eec5689f8d3d2b650a
zelr0x <zelr0xdev@gmail.com>
2019-03-06 15:27:04 +0400

    Added Extension - an enum that represents supported image types.

---
+++ internal/pkg/imgen/extension.go
@@ -0,0 +1,49 @@
+// Package imgen defines constants used by image generator
+// as well as methods of creating images with different parameters.
+// It also contains color palettes.
+package imgen
+
+import "strings"
+
+// Extension is an enumerator of supported image extensions.
+type Extension uint8
+
+const (
+	PNG Extension = iota
+	JPEG
+	GIF
+)
+
+// Mime returns a respective MIME-type string for a given Extension.
+func (e Extension) Mime() string {
+	main := "image/"
+	switch e {
+	case PNG:
+		return main + "png"
+	case JPEG:
+		return main + "jpeg"
+	case GIF:
+		return main + "gif"
+	default:
+		return main + "png"
+	}
+}
+
+// GetExtensionNames return a []string with available extensions.
+func GetExtensionNames() []string {
+	return []string{"png", "jpeg", "jpg", "gif"}
+}
+
+var extensions = map[string]Extension{
+	"png":  PNG,
+	"jpeg": JPEG,
+	"jpg":  JPEG,
+	"gif":  GIF,
+}
+
+// GetExtension tries to extract Extension from s.
+// Returns true on success, false otherwise.
+func GetExtension(s string) (Extension, bool) {
+	ext, exists := extensions[strings.ToLower(s)]
+	return ext, exists
+}

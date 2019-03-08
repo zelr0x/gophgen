fcee9d04dbd58f235bcf02719875860f6398031e
zelr0x <zelr0xdev@gmail.com>
2019-03-08 23:59:15 +0400

    Added web router (using gin gonic).

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -2,9 +2,34 @@
 // parses API requests and writes a response.
 package server
 
-import "fmt"
+import (
+	"os"
+	"path/filepath"
+	"strconv"
 
-func Serve(portNumber uint16) {
-	port := ":" + string(portNumber)
-	fmt.Println(port)
+	"github.com/gin-gonic/gin"
+)
+
+// setupRouter specifies available API routes.
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.NoRoute(func(c *gin.Context) {
+		c.File(filepath.Join(os.Getenv("GOPATH"),
+			"src/github.com/zelr0x/gophgen/web/static/index.html"))
+	})
+	r.GET("/:first", func(c *gin.Context) {
+		parseAndWrite(c, c.Param("first"), "")
+	})
+	r.GET("/:first/*rest", func(c *gin.Context) {
+		parseAndWrite(c, c.Param("first"), c.Param("rest"))
+	})
+
+	return r
+}
+
+// Serve starts the router.
+func Serve(port uint16) {
+	r := setupRouter()
+	r.Run(":" + strconv.Itoa(int(port)))
 }
